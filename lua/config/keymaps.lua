dc82e43dc631326526d1f0c1d581b596615b13df
linjiang <linjiang@micronet.ltd>
2023-11-01 09:34:22 +0800

    添加<leader>sw快捷键搜索当前光标所在单词

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,9 +15,7 @@ map("n", "<tab>", ">>", { desc = "input tab" })
 map("n", "=", "10j", { desc = "move down 10 line", noremap = true })
 map("n", "-", "10k", { desc = "move up 10 line", noremap = true })
 
-map("n", "<C-p>", "<leader>ff", { desc = "search file" })
 map("n", "<C-.>", "<leader>ca", { desc = "code action" })
-map("n", "<leader>p", "<leader>fb", { desc = "search buffer file" })
 
 --unbind Move Lines key
 map("n", "<A-j>", "", { desc = "cancel move down" })
@@ -32,3 +30,10 @@ map(
   ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
   { desc = "search with args" }
 )
+
+vim.keymap.set("n", "<leader>o", require("telescope.builtin").oldfiles, { desc = "search old files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").grep_string, { desc = "search current word" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "search help" })
+vim.keymap.set("n", "<leader>st", require("telescope.builtin").tags, { desc = "search tags" })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "search diagnostics" })
+vim.keymap.set("n", "<leader>s<space>", require("telescope.builtin").search_history, { desc = "search history" })
