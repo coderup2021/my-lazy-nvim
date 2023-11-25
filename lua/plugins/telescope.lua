8a7f4467e6747d5091e71b09a11d5f1fec420479
linjiang <linjiang@micronet.ltd>
2023-11-25 16:09:48 +0800

    添加prettier和black作为格式化的工具

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,49 @@
--- local lga_actions = require("telescope-live-grep-args.actions")
+--local lga_actions = require("telescope-live-grep-args.actions")
 
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     {
-        "nvim-telescope/telescope-live-grep-args.nvim" ,
-        -- This will not install any breaking changes.
-        -- For major updates, this must be adjusted manually.
-        version = "^1.0.0",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
     },
   },
-   -- extensions = {
-   --  live_grep_args = {
-   --    auto_quoting = true, -- enable/disable auto-quoting
-   --    -- define mappings, e.g.
-   --    mappings = { -- extend mappings
-   --      i = {
-   --        ["<C-k>"] = lga_actions.quote_prompt(),
-   --        ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-   --      },
-   --    },
-   --    -- ... also accepts theme settings, for example:
-   --    -- theme = "dropdown", -- use dropdown theme
-   --    -- theme = { }, -- use own theme spec
-   --    -- layout_config = { mirror=true }, -- mirror preview pane
-   --  }
-  -- },
   config = function()
     require("telescope").load_extension("live_grep_args")
-  end
+  end,
+  -- extensions = {
+  --   live_grep_args = {
+  --     auto_quoting = true, -- enable/disable auto-quoting
+  --     -- define mappings, e.g.
+  --     mappings = { -- extend mappings
+  --       i = {
+  --         ["<C-k>"] = lga_actions.quote_prompt(),
+  --         ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+  --       },
+  --     },
+  --     -- ... also accepts theme settings, for example:
+  --     -- theme = "dropdown", -- use dropdown theme
+  --     -- theme = { }, -- use own theme spec
+  --     -- layout_config = { mirror=true }, -- mirror preview pane
+  --   },
+  -- },
+  -- opts = {
+  --   defaults = {
+  --     vimgrep_arguments = {
+  --       "rg",
+  --       "--color=never",
+  --       "--no-heading",
+  --       "--line-number",
+  --       "--column",
+  --       "--smart-case",
+  --       "--hidden",
+  --       "--glob",
+  --       "!{**/.git/*,**/node_modules/*,**/pkg/mod/*,**/yarn.lock,**/.local/*}",
+  --     },
+  --   },
+  -- },
 }
+
+-- {$HOME}/.config/nvim/lua/plugins/telescope.lua
